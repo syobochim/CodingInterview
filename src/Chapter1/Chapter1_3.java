325098dd5fad32601b878ed89979795fc1de17fb
syobochim <MarilynMonroe0226@gmail.com>
2014-04-10 01:43:13 +0900

    add Chapter1_3 answer in book

--- src/Chapter1/Chapter1_3.java
+++ src/Chapter1/Chapter1_3.java
@@ -16,4 +16,26 @@ public class Chapter1_3 {
 
         return Arrays.equals(chr1, chr2);
     }
+
+    public static boolean isSameCharsForBook(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        int[] letters = new int[256]; //文字コードASCⅡの場合
+
+        char[] str1_array = str1.toCharArray();
+        for (char c : str1_array) {
+            letters[c]++;
+        }
+
+        for (int i = 0; i < str2.length(); i++) {
+            int c = (int) str2.charAt(i);
+            if (--letters[c] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
--- test/Chapter1/Chapter1_3Test.java
+++ test/Chapter1/Chapter1_3Test.java
@@ -16,6 +16,18 @@ public class Chapter1_3Test {
         assertThat(true, is(Chapter1_3.isSameChars("abc", "abc")));
         assertThat(false, is(Chapter1_3.isSameChars("abc", "cdba")));
         assertThat(false, is(Chapter1_3.isSameChars("abc", "def")));
+        assertThat(false, is(Chapter1_3.isSameChars("abC", "abc")));
     }
 
+    @Test
+    public void 二つの文字列の構成要素が同じであるか_本のアルゴリズム(){
+        assertThat(true, is(Chapter1_3.isSameCharsForBook("abc", "cba")));
+        assertThat(true, is(Chapter1_3.isSameCharsForBook("abc", "abc")));
+        assertThat(false, is(Chapter1_3.isSameCharsForBook("abc", "cdba")));
+        assertThat(false, is(Chapter1_3.isSameCharsForBook("abcd", "cba")));
+        assertThat(false, is(Chapter1_3.isSameCharsForBook("abc", "def")));
+        assertThat(false, is(Chapter1_3.isSameCharsForBook("abC", "abc")));
+        assertThat(false, is(Chapter1_3.isSameCharsForBook("aac", "aaa")));
+        assertThat(false, is(Chapter1_3.isSameCharsForBook("aaa", "aca")));
+    }
 }
