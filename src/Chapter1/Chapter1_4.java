8dacc06a1bc8c7e1f6aac29734fc9440114d8a6e
syobochim <MarilynMonroe0226@gmail.com>
2014-04-13 12:31:22 +0900

    add Chapter1_4 answer in Book

--- src/Chapter1/Chapter1_4.java
+++ src/Chapter1/Chapter1_4.java
@@ -8,4 +8,26 @@ public class Chapter1_4 {
     public static String encodeSpaces(String target) {
         return target.replaceAll("\\s+$", "").replaceAll("\\s", "%20");
     }
+
+    public static void encodeSpacesForBooks(char[] target, int length) {
+        int spaceCount = 0, newLength, i = 0;
+        for (i = 0; i < length; i++) {
+            if (target[i] == ' ') {
+                spaceCount++;
+            }
+        }
+        newLength = length + spaceCount * 2;
+//        target[newLength] = '\0';         // 本には記載があるけど、意味わからない＋実装するとArrayIndexOutOfBoundsException…。
+        for (i = length - 1; i >= 0; i--) {
+            if (target[i] == ' ') {
+                target[newLength - 1] = '0';
+                target[newLength - 2] = '2';
+                target[newLength - 3] = '%';
+                newLength -= 3;
+            } else {
+                target[newLength - 1] = target[i];
+                newLength--;
+            }
+        }
+    }
 }
--- test/Chapter1/Chapter1_4Test.java
+++ test/Chapter1/Chapter1_4Test.java
@@ -15,4 +15,11 @@ public class Chapter1_4Test {
         assertThat(Chapter1_4.encodeSpaces("Mr John Smith    "), is("Mr%20John%20Smith"));
     }
 
+    @Test
+    public void 文字列中のスペースをエンコードし末尾のスペースを削除している場合は真となること_本の答え() {
+        char[] target = "Mr John Smith    ".toCharArray();
+        char[] expect = "Mr%20John%20Smith".toCharArray();
+        Chapter1_4.encodeSpacesForBooks(target, 13);
+        assertThat(target, is(expect));
+    }
 }
