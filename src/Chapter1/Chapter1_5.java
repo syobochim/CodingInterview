31e3078672d7581c449fc47083367005664846b6
syobochim <MarilynMonroe0226@gmail.com>
2014-04-13 18:37:46 +0900

    綺麗にしようとしたらネスト増えた

--- src/Chapter1/Chapter1_5.java
+++ src/Chapter1/Chapter1_5.java
@@ -7,16 +7,19 @@ public class Chapter1_5 {
     public static String compressString(String target) {
         char[] targetChars = target.toCharArray();
         StringBuilder resultStringBuilder = new StringBuilder();
+
         for (int i = 0; i < targetChars.length; i++) {
             int sameCharCounter = 1;
-            for (int j = i + 1; j < target.length() && targetChars[i] == targetChars[j]; j++) {
+            for (int j = i + 1; j < target.length(); j++) {
+                if (targetChars[i] != targetChars[j]) break;
                 sameCharCounter++;
                 i = j;
             }
             resultStringBuilder.append(targetChars[i]);
             resultStringBuilder.append(sameCharCounter);
         }
-        if (resultStringBuilder.length() >= target.length()){
+
+        if (resultStringBuilder.length() >= target.length()) {
             return target;
         }
         return resultStringBuilder.toString();
