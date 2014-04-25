270b80f9135dfce755374b2eb57c942a0a3ffac1
syobochim <MarilynMonroe0226@gmail.com>
2014-04-25 09:01:59 +0900

    定数を切り出し

--- src/Chapter1/FizzBuzz.java
+++ src/Chapter1/FizzBuzz.java
@@ -1,16 +1,22 @@
 package Chapter1;
 
 public class FizzBuzz {
+
+    private static final int FIZZ_NUMBER = 3;
+    private static final int BUZZ_NUMBER = 5;
+    private static final String FIZZ_TEXT = "Fizz";
+    private static final String BUZZ_TEXT = "Buzz";
+
     public String convert(int number) {
 
-        if(isDivisible(number, 3 * 5)) {
-            return "FizzBuzz";
+        if(isDivisible(number, FIZZ_NUMBER * BUZZ_NUMBER)) {
+            return FIZZ_TEXT + BUZZ_TEXT;
         }
-        if(isDivisible(number, 3)){
-            return "Fizz";
+        if(isDivisible(number, FIZZ_NUMBER)){
+            return FIZZ_TEXT;
         }
-        if(isDivisible(number, 5)) {
-            return "Buzz";
+        if(isDivisible(number, BUZZ_NUMBER)) {
+            return BUZZ_TEXT;
         }
 
         return String.valueOf(number);
